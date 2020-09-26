63d931639de69fccaccd2c35c99b0aa9d4d5f498
Inscientor <scriptum_sacla@outlook.com>
2020-09-26 22:24:10 +0900

    作成

---
+++ SettingParser/CKParser.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingParser
+{
+    public class CKParser:IDisposable
+    {
+        private bool _disposed = false;
+
+        private StreamReader file;
+        public string FilePath { get; }
+
+        public CKParser(string filePath)
+        {
+            FilePath = filePath;
+            file = new StreamReader(FilePath);
+        }
+
+        public IEnumerable<SettingElement> ReadSettings()
+        {
+            var scope = new Stack<SettingElement>();
+            string line;
+            while((line = file.ReadLine()) != null)
+            {
+                // コメントを除去
+                var formula = line.Split('#')[0].Trim('\t');
+                // 空白行なら飛ばす
+                if (string.IsNullOrWhiteSpace(formula))
+                    continue;
+
+                // 関心の要素
+                var setElm = new SettingElement();
+                var data = formula.Split('=');
+
+                setElm.Name = data[0].Trim();
+                if (setElm.Name == "}")
+                {
+                    // スコープから出る
+                    var elm = scope.Pop();
+
+                    // 上位にポップした値を投げる
+                    if (scope.Any())
+                        scope.Peek().Attributes.Add(elm);
+                    else
+                        yield return elm;
+
+                    continue;
+                }
+
+                string value = data[1].Trim();
+                if (value == "{")
+                {
+                    // スコープに入る
+                    scope.Push(setElm);
+                }
+                else
+                {
+                    // リテラルを作る
+                    setElm.Attributes.Add(new SettingElement() { Name = value });
+
+                    // スコープに格納する
+                    if (scope.Any())
+                        scope.Peek().Attributes.Add(setElm);
+                    else
+                        yield return setElm;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    file.Dispose();
+                }
+
+                _disposed = true;
+            }
+        }
+    }
+}
---
+++ SettingParser/SettingElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingParser
+{
+    public class SettingElement
+    {
+        public string Name { get; set; }
+        public List<SettingElement> Attributes { get; } = new List<SettingElement>();
+        public bool HasAttribute => Attributes.Any();
+        public bool IsLiteral => Attributes.Count == 1 && !Attributes.First().HasAttribute;
+
+        public override string ToString() => ToString();
+
+        public string ToString(string prepend = "") => IsLiteral
+                ? $"{prepend}{Name}={Attributes.First().Name}"
+                : Attributes.Aggregate($"{prepend}{Name}={{", (sum, elm) => $"{sum}\r\n{elm.ToString($"{prepend}\t")}") + $"\r\n{prepend}}}";
+    }
+}
